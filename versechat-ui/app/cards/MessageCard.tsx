8b9fb49f01bed5b2312acf078063af878ecc4653
Amulya Prasanth <amulyaprasanth301@gmail.com>
2026-08-20 14:00:34 +0530

    Revert "moved message loader widget to message card"
    This reverts commit 615110669eb890e5395a246edc6d4f52130f363f.

--- versechat-ui/app/cards/MessageCard.tsx
+++ versechat-ui/app/cards/MessageCard.tsx
@@ -2,28 +2,15 @@
 import { Message } from "../types/chat";
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import MessageLoader from "./MessageLoading";
 
-
-type MessageCardProps = {
-  message: Message;
-  isLoading: boolean;
-}
-
-const MessageCard = ({ message, isLoading }: MessageCardProps) => {
+const MessageCard = ({ role, content }: Message) => {
   return (
     <div
-      className={`max-w-3xl p-2 rounded-2xl ${message.role === "assistant" ? "self-start text-white text-l/5" : "self-end bg-stone-100 text-black rounded-br-none"}`}>
-      {
-        isLoading && (message.role == "assistant") ? (<div className="w-4 h-4 self-start"> <MessageLoader /></div>)
-          :
-          < ReactMarkdown remarkPlugins={[remarkGfm]}>
-            {message.content}
-          </ReactMarkdown>
-
-      }
-
-    </div >
+      className={`max-w-3xl p-2 rounded-2xl ${role === "assistant" ? "self-start text-white text-l/5" : "self-end bg-stone-100 text-black rounded-br-none"}`}>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>
+        {content}
+      </ReactMarkdown>
+    </div>
   );
 };
 
